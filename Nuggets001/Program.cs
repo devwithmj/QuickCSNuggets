950df0d6970f9b5e82cec3204c2979e4d898f6f2
Majid Alahverdi <majidalahverdi@gmail.com>
2025-08-03 21:53:58 -0400

    Initialize Nuggets001 .NET 6 console project
    Add initial solution, project files, and .gitignore for a .NET 6 console application. Includes a basic Program.cs that prints a message and serializes a JSON object.
    C# Nugget 001

---
+++ Nuggets001/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+var message = JsonSerializer.Serialize(new { Message = "Hello World!" });
+Console.WriteLine(message);
