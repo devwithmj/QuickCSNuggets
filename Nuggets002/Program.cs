2474c363bec0e6dbc42d4641cf628c3854af2b6d
Majid Alahverdi <majidalahverdi@gmail.com>
2026-01-03 15:16:38 -0500

    Add initial Nuggets002 C# project
    Created a new C# console project targeting .NET 8.0 with a sample Program.cs demonstrating usage of var and a Customer record. Added the corresponding solution and project files.

---
+++ Nuggets002/Program.cs
@@ -0,0 +1,11 @@
+﻿//Quick C# Nuggets #2
+//When NOT to use `var
+//Youtuber: devwithmj
+//youtube.com/devwithmj
+//github.com/devwithmj
+
+var client = new HttpClient();
+
+var customers = new List<Customer>();
+
+record Customer(string Name, string City);
